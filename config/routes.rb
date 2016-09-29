8fec6933a68e7e619bc56d3c50a6d9e95cca8050
maximederoeux <maxime.deroeux@gmail.com>
2016-09-29 18:25:04 +0200

    Idem

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
 
   if Rails.env.production?
     match '/', to: 'users#index', constraints: { subdomain: '' }, via: [:get, :post, :put, :patch, :delete]
-  	match '/', to: 'visitors#index', constraints: { subdomain: /.+/ }, via: [:get, :post, :put, :patch, :delete]
+  	match '/', to: 'visitors#index', constraints: { subdomain: /[^spekuloozdev].+/ }, via: [:get, :post, :put, :patch, :delete]
   else
     match '/', to: 'users#index', constraints: { subdomain: '' }, via: [:get, :post, :put, :patch, :delete]
   	match '/', to: 'visitors#index', constraints: { subdomain: /.+/ }, via: [:get, :post, :put, :patch, :delete]
