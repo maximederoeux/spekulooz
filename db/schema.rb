8389120275e9dd8073cd8a09f19a2c1cd1795e75
maximederoeux <maxime.deroeux@gmail.com>
2016-09-29 15:28:16 +0200

    Added items

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160929131444) do
+ActiveRecord::Schema.define(version: 20160929132716) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "subdomain"
     t.string   "resto_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.integer  "account_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
