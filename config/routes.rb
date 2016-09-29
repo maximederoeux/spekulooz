44ba2e8d634df49517771ecf88173b744d70573d
maximederoeux <maxime.deroeux@gmail.com>
2016-09-29 17:49:46 +0200

    Idem

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
   resources :accounts
   resources :users
 
-	match '/', to: 'visitors#index', constraints: { subdomain: '' }, via: [:get, :post, :put, :patch, :delete]
-  match '/', to: 'accounts#show', constraints: { subdomain: /.+/ }, via: [:get, :post, :put, :patch, :delete]
+	match '/', to: 'users#index', constraints: { subdomain: '' }, via: [:get, :post, :put, :patch, :delete]
+  match '/', to: 'visitors#index', constraints: { subdomain: /.+/ }, via: [:get, :post, :put, :patch, :delete]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
