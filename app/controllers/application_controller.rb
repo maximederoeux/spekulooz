09cb965b417d7387838b0e9178e42913846e0580
maximederoeux <maxime.deroeux@gmail.com>
2016-09-29 17:46:11 +0200

    Idem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-  before_filter :get_account
+#   before_filter :get_account
 
-private
-  def get_account
+# private
+#   def get_account
 
-    @account = Account.where(subdomain: request.subdomain).first
+#     @account = Account.where(subdomain: request.subdomain).first
 
-  end
+#   end
 
 end
