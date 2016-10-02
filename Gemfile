bf4766ce8478be90611afdc0dd8652bd7c348936
maximederoeux <maxime.deroeux@gmail.com>
2016-10-02 06:42:42 +0200

    Internalization

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'simple_form'
 gem 'devise'
 gem 'bootstrap', '~> 4.0.0.alpha4'
 gem 'rails-i18n', '~> 5.0.0'
+gem 'devise-i18n'
+
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_sub_account
-#   before_filter :get_account
+  before_action :set_locale
 
-# private
-#   def get_account
-
-#     @account = Account.where(subdomain: request.subdomain).first
-
-#   end
 
   def default_url_options
     if Rails.env.production?
@@ -21,6 +15,20 @@ class ApplicationController < ActionController::Base
   def set_sub_account
   	@sub_accounts = Account.where(subdomain: request.subdomain).all
   	@sub_account = @sub_accounts.first  	
+  end
+
+  def set_locale
+  I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options(options = {})
+    logger.debug "default_url_options is passed options: #{options.inspect}\n"
+    { :locale => I18n.locale }
+  end
+
+  def extract_locale_from_tld
+    parsed_locale = request.host.split('.').last
+    I18n.available_locales.map(&:to_s).include?(parsed_locale) ? parsed_locale : nil
   end
 
 end
