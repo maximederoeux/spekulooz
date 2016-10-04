f0dab7aedb1b0b4f95d9b5bbf35b14d8e4e9c4b0
maximederoeux <maxime.deroeux@gmail.com>
2016-10-04 12:36:27 +0200

    Same issue

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,13 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_spekulooz_session', :domain => :all
+# Rails.application.config.session_store :cookie_store, key: '_spekulooz_session', :domain => :all
+
+# FROM http://stackoverflow.com/questions/6301121/rails-how-can-i-share-permanent-cookies-across-multiple-subdomains
+
+if Rails.env.production?
+  Rails.application.config.session_store :cookie_store, :key => '_app_name_session', :domain => '.spekulooz.be'
+else
+  Rails.application.config.session_store :cookie_store, :key => '_app_name_session'
+end
+
+# END FROM
