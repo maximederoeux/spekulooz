1e153be2ccba4f249a602dbbfdd29624389adae0
maximederoeux <maxime.deroeux@gmail.com>
2016-10-04 12:26:40 +0200

    Sessions break between domain and subdomains

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,16 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_spekulooz_session', :domain => :all
+if Rails.env.development?
+
+	Rails.application.config.session_store :cookie_store, key: '_spekulooz_session', :domain => "lvh.me"
+
+else
+
+	Rails.application.config.session_store :cookie_store, key: '_spekulooz_session', :domain => :all
+
+
+end
+
+
+
+
