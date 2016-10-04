1dc1d5b82a64bc97450409bda329928dad30bfa3
maximederoeux <maxime.deroeux@gmail.com>
2016-10-04 12:28:29 +0200

    Idem

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,16 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-if Rails.env.development?
-
-	Rails.application.config.session_store :cookie_store, key: '_spekulooz_session', :domain => "lvh.me"
-
-else
-
-	Rails.application.config.session_store :cookie_store, key: '_spekulooz_session', :domain => :all
-
-
-end
-
-
-
-
+Rails.application.config.session_store :cookie_store, key: '_spekulooz_session', :domain => :all
